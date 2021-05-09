60fbe78f3bbeb4856b5cd9877ab6e6cc48e90cd4
yxxung <loveus011@naver.com>
2021-05-09 20:59:46 +0900

    Revert "메모 작성기능 추가"
    This reverts commit 73bf71a1884063ac45630ad16814fd00f2ebdb5d.

--- 0430/CBNU_03_Android/app/src/main/java/com/example/cbnu_03_android/MainActivity.java
+++ 0430/CBNU_03_Android/app/src/main/java/com/example/cbnu_03_android/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.cbnu_03_android;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import java.text.SimpleDateFormat;
@@ -12,25 +12,47 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public int MONTH_COUNT = 0;
+    /**
+     * 연/월 텍스트뷰
+     */
     private TextView tvDate;
+    /**
+     * 그리드뷰 어댑터
+     */
     private GridAdapter gridAdapter;
+
+    /**
+     * 일 저장 할 리스트
+     */
     private ArrayList<String> dayList;
+
+    /**
+     * 그리드뷰
+     */
     private GridView gridView;
+
+    /**
+     * 캘린더 변수
+     */
     private Calendar mCal;
-    private Button left_press, right_press, goMemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,55 +62,18 @@ public class MainActivity extends AppCompatActivity {
         tvDate = (TextView)findViewById(R.id.tv_date);
         gridView = (GridView)findViewById(R.id.gridview);
 
-        left_press = (Button)findViewById(R.id.left_press);
-        right_press = (Button)findViewById(R.id.right_press);
-
-        goMemo = (Button)findViewById(R.id.goMemo);
-
-
-        left_press.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MONTH_COUNT=MONTH_COUNT-1;
-                //new_month(MONTH_COUNT);
-            }
-        });
-
-        right_press.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MONTH_COUNT=MONTH_COUNT+1;
-                //new_month(MONTH_COUNT);
-            }
-        });
-
-        goMemo.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), MemoActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        new_month(MONTH_COUNT);
-    }
-
-    private void new_month(int cnt)
-    {
-
+        // 오늘에 날짜를 세팅 해준다.
         long now = System.currentTimeMillis();
         final Date date = new Date(now);
-
-
+        //연,월,일을 따로 저장
         final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
         final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
         final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
 
-        final SimpleDateFormat curMonthFormat2 = new SimpleDateFormat("MMMM", Locale.US);
+        //현재 날짜 텍스트뷰에 뿌려줌
+        tvDate.setText(curYearFormat.format(date) + "/" + curMonthFormat.format(date));
 
-        tvDate.setText(curMonthFormat2.format(date));
-
+        //gridview 요일 표시
         dayList = new ArrayList<String>();
         dayList.add("일");
         dayList.add("월");
@@ -100,9 +85,10 @@ public class MainActivity extends AppCompatActivity {
 
         mCal = Calendar.getInstance();
 
+        //이번달 1일 무슨요일인지 판단 mCal.set(Year,Month,Day)
         mCal.set(Integer.parseInt(curYearFormat.format(date)), Integer.parseInt(curMonthFormat.format(date)) - 1, 1);
         int dayNum = mCal.get(Calendar.DAY_OF_WEEK);
-
+        //1일 - 요일 매칭 시키기 위해 공백 add
         for (int i = 1; i < dayNum; i++) {
             dayList.add("");
         }
@@ -110,8 +96,54 @@ public class MainActivity extends AppCompatActivity {
 
         gridAdapter = new GridAdapter(getApplicationContext(), dayList);
         gridView.setAdapter(gridAdapter);
+
+        // 0502 이명국_수정 Gridview 클릭시
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // 날짜 클릭시 리스트 확인을 위한 AlertDialog 띄우기
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("");
+                builder.setMessage("오늘의 일정 입니다.");
+                final EditText editText = new EditText(MainActivity.this);
+                // 일정이 표시될 textView, 추후 listView로 변경 예정
+                final TextView textView = new TextView(MainActivity.this);
+                // 리스트 받아올 함수 작성 예정.
+
+                // 추가 버튼 클릭시 새 AlertDialog를 통한 edittext
+                builder.setPositiveButton("추가", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AlertDialog.Builder builder2 = new AlertDialog.Builder(MainActivity.this);
+                        builder2.setTitle("일정을 입력하세요");
+                        builder2.setView(editText);
+
+                        // 일정 입력 후 builder의 list에 추가 기능 미구현
+                        builder2.setPositiveButton("일정 추가", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), editText.getText() , Toast.LENGTH_LONG).show();
+                                // eidtText 값을 리스트에 추가(builder에 띄우기 위함)
+                            }
+                        });
+                        builder2.setNegativeButton("취소",null);
+                        builder2.create().show();
+
+                    }
+                });
+                builder.setNegativeButton("취소",null);
+                builder.create().show();
+            }
+        });
+        //0502 이명국_수정 끝
     }
 
+    /**
+     * 해당 월에 표시할 일 수 구함
+     *
+     * @param month
+     */
     private void setCalendarDate(int month) {
         mCal.set(Calendar.MONTH, month - 1);
 
@@ -121,11 +153,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * 그리드뷰 어댑터
+     *
+     */
     private class GridAdapter extends BaseAdapter {
 
         private final List<String> list;
+
         private final LayoutInflater inflater;
 
+        /**
+         * 생성자
+         *
+         * @param context
+         * @param list
+         */
         public GridAdapter(Context context, List<String> list) {
             this.list = list;
             this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -156,38 +199,18 @@ public class MainActivity extends AppCompatActivity {
                 holder = new ViewHolder();
 
                 holder.tvItemGridView = (TextView)convertView.findViewById(R.id.tv_item_gridview);
-                convertView.setTag(holder);
 
+                convertView.setTag(holder);
             } else {
                 holder = (ViewHolder)convertView.getTag();
             }
             holder.tvItemGridView.setText("" + getItem(position));
 
-
-
+            //해당 날짜 텍스트 컬러,배경 변경
             mCal = Calendar.getInstance();
-
+            //오늘 day 가져옴
             Integer today = mCal.get(Calendar.DAY_OF_MONTH);
             String sToday = String.valueOf(today);
-
-            if (position<7)
-            {
-                holder.tvItemGridView.setTextColor(Color.parseColor("#9966CC"));
-            }
-            else if((position+1)%7==0)
-            {
-                holder.tvItemGridView.setTextColor(Color.parseColor("#3300CC"));
-            }
-            else if(position%7==0)
-            {
-                holder.tvItemGridView.setTextColor(Color.parseColor("#FF0033"));
-            }
-
-            if (sToday.equals(getItem(position))) {
-                holder.tvItemGridView.setTextColor(Color.parseColor("#663399"));
-            }
-
-
 
             return convertView;
         }
