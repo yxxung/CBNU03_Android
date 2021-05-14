31357215f7fc613b9b1fa55563fa72132bc4e7ad
jehyeon <s85737@gmail.com>
2021-05-14 14:33:50 +0900

    firestore 에서 realtime database로 변경 Test성공

--- 0430/CBNU_03_Android/app/src/main/java/com/example/cbnu_03_android/ConnectDBActivity.java
+++ 0430/CBNU_03_Android/app/src/main/java/com/example/cbnu_03_android/ConnectDBActivity.java
@@ -7,17 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class ConnectDBActivity extends Activity {
 
-
+    private DatabaseReference db;
     Button dbTestBtn;
     Button dbAddBtn;
     EditText textMemo;
@@ -31,6 +34,8 @@ public class ConnectDBActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_db);
+
+        db = FirebaseDatabase.getInstance().getReference();
         dbTestBtn = findViewById(R.id.button_DB_Test);
         dbAddBtn = findViewById(R.id.button_add_to_DB);
         textDate = findViewById(R.id.input_date);
@@ -45,10 +50,27 @@ public class ConnectDBActivity extends Activity {
             public void onClick(View view) {
 
                 int isDone = 0;
+                String id = "1";
                 String addMemo = textMemo.getText().toString();
                 String addDate = textDate.getText().toString();
-
                 newMemo = new Memo(addMemo, addDate, isDone);
+
+                db.child("memo").child(id).setValue(newMemo)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Write was successful!
+                        Toast.makeText(ConnectDBActivity.this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        Toast.makeText(ConnectDBActivity.this, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                });
 
 
             }
