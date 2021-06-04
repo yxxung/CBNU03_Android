1d73fbd58768866199ec9c2b04775701f0ccef86
jehyeon <s85737@gmail.com>
2021-06-04 21:49:21 +0900

    중복 회원가입 방지

--- 0430/CBNU_03_Android/app/src/main/java/com/example/cbnu_03_android/SignUpActivity.java
+++ 0430/CBNU_03_Android/app/src/main/java/com/example/cbnu_03_android/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.cbnu_03_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,8 +9,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -44,22 +48,40 @@ public class SignUpActivity extends AppCompatActivity {
     private void signUp() {
 
         String id = idText.getText().toString();
-        String password = pwText.getText().toString();
-        String name = nameText.getText().toString();
-        String phoneNumber = contactText.getText().toString();
-        String passwordCheck = pwCheckText.getText().toString();
 
-        User user = new User(id, password, name, phoneNumber);
 
-        if(id.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
-            if(password.equals(passwordCheck)){
-                db.child("userList").child(id).setValue(user);
-                Toast.makeText(this, "로그인 성공!", Toast.LENGTH_SHORT).show();
-                onBackPressed();
-            } else{
-                Toast.makeText(this, "비밀번호가 일치하지 않음!", Toast.LENGTH_SHORT).show();
+        db.child("userList").child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if(user == null){
+                    String id = idText.getText().toString();
+                    String password = pwText.getText().toString();
+                    String name = nameText.getText().toString();
+                    String phoneNumber = contactText.getText().toString();
+                    String passwordCheck = pwCheckText.getText().toString();
+                    User newUser = new User(id, password, name, phoneNumber);
+                    if(id.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
+                        if(password.equals(passwordCheck)){
+                            db.child("userList").child(id).setValue(newUser);
+                            Toast.makeText(getApplicationContext(), "로그인 성공!", Toast.LENGTH_SHORT).show();
+                            onBackPressed();
+                        } else{
+                            Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않음!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(), "동일한 아이디가 존재합니다.", Toast.LENGTH_SHORT).show();
+                }
             }
-        }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
 
